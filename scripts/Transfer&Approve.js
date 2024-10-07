e4ffd55d4d2dc9d77c02fe8ff299e96f37e93db3
craftersdeveloper <raghavprabhkar@gmail.com>
2024-10-07 23:28:46 +0530

    Update Transfer&Approve.js

--- scripts/Transfer&Approve.js
+++ scripts/Transfer&Approve.js
@@ -1,6 +1,6 @@
 const hre = require("hardhat");
 const fxRootContractABI = require("../fxRootContractABI.json");
-const tokenContractJSON = require("../artifacts/contracts/MonkeyRush.sol/MonkeyRush.json");
+const tokenContractJSON = require("../artifacts/contracts/MonkeyMarket.sol/MonkeyMarket.json");
 
 const tokenAddress = process.env.ContractAddress;
 const tokenABI = tokenContractJSON.abi;
@@ -18,22 +18,34 @@ async function main() {
       FxERC721RootTunnel
     );
 
+    const tokenIds = [0, 1, 2, 3, 4];
+
     const approveTx = await tokenContract.setApprovalForAll(
       FxERC721RootTunnel,
       true
     );
     await approveTx.wait();
-    
     console.log("Approval confirmed successfully.");
-    console.log("Tokens transferred successfully.");
-    console.log("Fetching balances for confirmation...");
+    console.log("Tokens transfer Process Initiated.");
+    
 
+    for (let i = 0; i < tokenIds.length; i++) {
+      const depositTx = await fxContract.deposit(
+        tokenAddress,
+        walletAddress,
+        tokenIds[i],
+        "0x6556"
+      );
+      await depositTx.wait();
+      console.log(`Token with ID ${tokenIds[i]} deposited`);
+    }
+
+    // Test balanceOf
     const balance = await tokenContract.balanceOf(walletAddress);
-    console.log(`You now have ${balance} NFTs in your wallet.`);
+    console.log("Fetching balances for confirmation...");
+    console.log(`You now have: ${balance} NFTs in your wallet For Sepolia Chain.`);
   } catch (error) {
-
-    console.error("An error occurred:", error);
-    process.exit(1);
+    console.error(error);
     process.exitCode = 1;
   }
 }
